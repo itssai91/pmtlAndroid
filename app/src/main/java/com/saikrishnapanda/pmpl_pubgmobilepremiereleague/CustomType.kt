9bda6e17bd8abe32ecd353f721d56b7e4d0bbb8b
Saikrishna Panda <itssai91@gmail.com>
2020-10-29 12:08:08 +0530

    Project not Complete this is the initial Prototype

---
+++ app/src/main/java/com/saikrishnapanda/pmpl_pubgmobilepremiereleague/CustomType.kt
@@ -0,0 +1,11 @@
+package com.saikrishnapanda.pmpl_pubgmobilepremiereleague
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomType : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_type)
+    }
+}
